8bd60c9055b6481ad602a4a94373c5bd3c3fd7ba
liuyuxi520 <2805242676@qq.com>
2018-03-01 13:42:37 +0800

    add sort function

--- test.py
+++ test.py
@@ -80,28 +80,15 @@ print('计算tup3的长度:',len(tup3))
 list5= ['Google', 'Taobao', 'Runoob', 'Baidu']
 tuple5=tuple(list5)
 print('tuple()将列表转化为元组:',list5,tuple5)
-# 5.函数，杨辉三角
-print('5.函数，杨辉三角')
-def yhFn(n,m):
-    if(m > n):
-        return false
-    if(m == 1 and n == 1):
-        return 1
-    if(m == 1 or m == n):
-        return 1
-    if(n > 2 and (m == 2 or m == n-1)):
-        return n-1
-    return yhFn(n-1,m-1) + yhFn(n-1,m)
-print('yhsj',yhFn(7,3),yhFn(7,4),yhFn(7,5),yhFn(8,4),yhFn(10,3))
 
-# 6.时间，日历
-print('6.时间，日历')
+# 7.时间，日历
+print('7.时间，日历')
 import calendar
 cal = calendar.month(2018, 2)
 print("以下输出2018年2月份的日历:")
 print(cal)
-# 7.条件控制
-print('7.条件控制')
+# 8.条件控制
+print('8.条件控制')
 age = int(input('请输入你家狗狗的年龄: '))
 if age < 0:
     print('你是在逗我吧!')
@@ -113,8 +100,8 @@ elif age > 2:
     human = 22 + (age - 2) * 5
     print('对应人类年龄: ', human)
 
-# 8.循环语句
-print('8.循环语句')
+# 9.循环语句
+print('9.循环语句')
 count = 0
 while count < 4:
     print ('count:',count)
@@ -122,21 +109,127 @@ while count < 4:
 else:
     print ('大于或等于4count:',count)
 
-#9.迭代器，生成器
-print('9.迭代器，生成器')
+#10.迭代器，生成器
+print('10.迭代器，生成器')
 list3=[1,2,3,4]
 it = iter(list3)    # 创建迭代器对象
 for x in it:
-    print (x)
+    print ('iter',x)
+
 def foo2():
     print('begin')
     for i in range(5):
-        print('before yield',i)
+        # print('before yield',i)
         yield i
-        print('after yield',i)
+        # print('after yield',i)
     print('end')        
-f = foo2()
-f.next() #begin <br> ('before yield',0)
-f.next() #('after yield',0) <br> ('before yield',1)
+ff = foo2()
+for i in ff:
+    print ('oo',i)
 
+# print(next(f))#begin <br> ('before yield',0)
+# print(next(f)) #('after yield',0) <br> ('before yield',1)
 
+#11.print杨辉三角
+print('11.print杨辉三角')
+
+def yhsjFn(n):
+    L = [1]
+    while True:
+        if n < len(L):
+            return
+        yield L
+        L.append(0)  
+        L = [L[i-1]+L[i] for i in range(len(L))]  
+yhsj = yhsjFn(4)
+print ('杨辉三角')  
+for i in yhsj:
+    print (i)  
+# while True:
+#     try:
+#         print (next(f))
+#     except StopIteration:
+#         sys.exit()
+
+def yhsjFn2(n):
+    Li = [1]
+    while True:
+        if n < len(Li):
+            return
+        yield Li
+        Li = [1]+[Li[i] + Li[i+1] for i in range(len(Li)-1)]+[1]
+f2 = yhsjFn2(5)
+
+print ('杨辉三角2')   
+for i in f2:
+    print (i)   
+
+# while True:
+#     try:
+#         print (next(f2))
+#     except StopIteration:
+#         sys.exit() 
+
+# 12.函数，杨辉三角求某个位置值
+print('12.函数，杨辉三角求某个位置值')
+def yhFn(n,m):
+    if(m > n):
+        return false
+    if(m == 1 and n == 1):
+        return 1
+    if(m == 1 or m == n):
+        return 1
+    if(n > 2 and (m == 2 or m == n-1)):
+        return n-1
+    return yhFn(n-1,m-1) + yhFn(n-1,m)
+print('杨辉三角位置值',yhFn(7,3),yhFn(7,4),yhFn(7,5),yhFn(8,4),yhFn(10,3))       
+
+# 13.斐波那契
+print ('13.斐波那契')
+def fibonacci(n): # 生成器函数 - 斐波那契
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n): 
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+f = fibonacci(10) # f 是一个迭代器，由生成器返回生成
+for i in f:
+    print (i,end=' ') 
+# while True:
+#     try:
+#         print (next(f), end=" ")
+#     except StopIteration:
+#         sys.exit()
+
+# 14.冒泡排序
+print ('\n14.冒泡排序')
+def buddleSort(lists):
+    count = len(lists)
+    if count <= 0:
+        return '请输入有值列表'
+    if count == 1:
+        return lists
+    for i in range(0,count):
+        for j in range(i+1,count):
+            if lists[i] > lists[j]:
+                lists[i],lists[j] = lists[j],lists[i]
+    return lists            
+print('冒泡排序：',buddleSort([2,5,1,3]))
+
+# 15.选择排序
+print ('15.选择排序')
+def selectSort(lists):
+    listLen = len(lists)
+    if listLen <= 0:
+        return '请输入有值列表'
+    if listLen == 1:
+        return lists
+    for i in range(0,listLen):
+        for j in range(i+1,listLen):
+            if lists[j] < lists[i]:
+                lists[i],lists[j] = lists[j],lists[i]
+    return lists  
+print('选择排序：',selectSort([8,5,2,3,1,3]))
+    
